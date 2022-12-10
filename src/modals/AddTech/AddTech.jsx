67166664fce918222891f16d36241da133757bf8
Gabriel <gabpavao@outlook.com>
2022-12-10 12:13:20 -0300

    fix: import incorrection

--- src/modals/AddTech/AddTech.jsx
+++ src/modals/AddTech/AddTech.jsx
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import { TechContext } from "../../contexts/TechContext";
 import { StyledTitle } from "../../Styles/typography";
 import { StyledBackgroundModal, StyledContainerModal } from "../StyledModal";
-import { AddTechForm } from "./AddTechForm/addTechForm";
+import { AddTechForm } from "./AddTechForm/AddTechForm";
 
 export function AddTechModal () {
     const { setIsAddTechModalOpen } = useContext(TechContext)
